73e368ff675062c30dcde0b52da9dea0e8a3ad1d
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 20:23:54 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,13 @@ class SessionsController < ApplicationController
   def create
     #You said you're 'username', huh? Let's see if you're on the list...
     @user=User.find_by(name: params[:name])
-
-    #You're forbidden unless the password you supplied in the params(when salted and hashed)  matches that
-    #which was previously salted and hashed for your username in the database
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    #if you passed the previous step, here's your wristband. Drink away and don't be an idiot.
-    session[:user_id] = @user.id
+    if @user
+      #You're forbidden unless the password you supplied in the params(when salted and hashed)  matches that
+      #which was previously salted and hashed for your username in the database
+      return head(:forbidden) unless @user.authenticate(params[:password])
+      #if you passed the previous step, here's your wristband. Drink away and don't be an idiot.
+      session[:user_id] = @user.id
+    else
+      redirect_to login_path
   end
 end
