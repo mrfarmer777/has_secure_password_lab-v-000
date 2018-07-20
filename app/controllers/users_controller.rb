f5c310b4e7b950f83799ed7ea6d109a59273cdad
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 19:03:53 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+    #instantiate with strong_params, save to db
+    user = User.new(user_params).save
+
+  end
+
 end
