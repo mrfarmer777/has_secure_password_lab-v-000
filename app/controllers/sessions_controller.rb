c675b4e965df22f1db82d963c2e486b3532b977e
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 20:34:58 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     #You said you're 'username', huh? Let's see if you're on the list...
-    
+    raise params.inspect
     @user=User.find_by(name: params[name])
     if @user
       #You're forbidden unless the password you supplied in the params(when salted and hashed)  matches that
