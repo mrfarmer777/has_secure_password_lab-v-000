7987143366c88e4221b1701cc4c270da4b63f413
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 19:12:20 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController
+  #this is like the bouncer, asks for ID, checks against criteria and creates a session (wristband)
 
+  def create
+    #You said you're 'username', huh? Let's see if you're on the list...
+    @user=User.find_by(username: params[:username])
+
+    #You're forbidden unless the password you supplied in the params(when salted and hashed)  matches that  
+    #which was previously salted and hashed for your username in the database
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    #if you passed the previous step, here's your wristband. Drink away and don't be an idiot.
+    session[:user_id] = @user.id
+  end
 end
