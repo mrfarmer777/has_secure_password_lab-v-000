74cee25eb9cbd05375b1b26a06568d38de40db59
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 19:06:00 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,18 @@ class UsersController < ApplicationController
 
   def create
     #instantiate with strong_params, save to db
+    #one of these params is password and password_confirmation, I believe when the User class creates a new
+    #user, it will use the has_secure_password macro to create the password digest before actually saving it....
+    #this should be baked in
     user = User.new(user_params).save
 
   end
 
-  prive 
+  prive
 
   #strong params function
   def user_params
+    #the form is sending back a user hash within params taht includes username, passowrd, and password_confirmation
     params.require(:user).permite(:username, :password, :password_confirmation)
   end
 
